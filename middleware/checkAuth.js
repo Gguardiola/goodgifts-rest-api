56fb5a9b43104f9fbdbb39e74f12a7a8d5398cdf
Gx3 Studios <gabriel.guardiola@estudiantat.upc.edu>
2023-12-27 23:11:15 +0100

    Update checkAuth.js

--- middleware/checkAuth.js
+++ middleware/checkAuth.js
@@ -5,10 +5,10 @@ const AUTH_SERVICE_HOST = process.env.AUTH_SERVICE_HOST;
 //Explanation: This is a middleware function that will be used to validate the token sent by the client.
 module.exports = async (req, res, next) => {
 
-    const {authorization} = req.headers.authorization;
+    const authorization = req.headers.authorization;
 
     if (!authorization) {
-        return res.status(402).json({ success: false, message: 'Token not provided' });
+        return res.status(401).json({ success: false, message: 'Token not provided' });
     }
 
     try {
