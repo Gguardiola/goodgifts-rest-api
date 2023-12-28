d8de2c04a63dcb894fab446e90eab914abfb0796
Gx3 Studios <gabriel.guardiola@estudiantat.upc.edu>
2023-12-28 01:03:38 +0100

    Update requestLimiter.js

--- middleware/requestLimiter.js
+++ middleware/requestLimiter.js
@@ -1,8 +1,8 @@
 const rateLimit = require('express-rate-limit');
 
 const requestLimiter = rateLimit({
-    windowMs: 15 * 60 * 1000, // 15 minutes
-    max: 5, // 5 requests per windowMs
+    windowMs: 15 * 60 * 1000,
+    max: 50,
     message: 'Too many requests from this IP, please try again later.',
 });
 
