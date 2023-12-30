21da6b01f86de00478801b0623b6a1b38117ea22
Gx3 Studios <gabriel.guardiola@estudiantat.upc.edu>
2023-12-30 18:02:58 +0100

    Update auth.js

--- routes/auth.js
+++ routes/auth.js
@@ -6,6 +6,8 @@ if(process.env.NODE_ENV != "production") require('dotenv').config();
 const AUTH_SERVICE_HOST = process.env.AUTH_SERVICE_HOST; 
 //Explanation: These are the routes that will be used by the client to signup and login.
 
+const dbUsers = require('../database/dbUsers');
+const dbWishlists = require('../database/dbWishlists');
 // POST /auth/signup
 router.post('/signup', requestLimiter, async (req, res) => {
     const { email, password, username, lastname, birthday } = req.body;
@@ -16,6 +18,8 @@ router.post('/signup', requestLimiter, async (req, res) => {
             return res.status(response.status).json(response.data);
         }
         res.json({success: true, message: 'Signup successful'});
+        const currentUserId = await dbUsers.retrieveUserId(email);
+        dbWishlists.createWishlist(currentUserId.rows[0].id, "My Wishlist");
 
     } catch (error) {
         console.error('Error:', error.message);
