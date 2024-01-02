55a90acfbbdf2930f23be0d278ad735aa6876655
Gx3 Studios <gabriel.guardiola@estudiantat.upc.edu>
2024-01-03 00:20:50 +0100

    Update gifts.js

--- database/gifts.js
+++ database/gifts.js
@@ -2,25 +2,25 @@ const db = require('./db');
 
 const retrieveAllUserGifts = async (requestedUser, userId, limit, offset) => {
     userId = userId.replace(/^"|"$/g, '');
-    result = await db.query('SELECT * FROM gifts WHERE id = (SELECT gift_id FROM user_gifts WHERE user_id = $2) AND gifted_user_id != $1 LIMIT $3 OFFSET $4', [requestedUser, userId, limit, offset]);
+    const result = await db.query('SELECT * FROM gifts WHERE id = (SELECT gift_id FROM user_gifts WHERE user_id = $2) AND gifted_user_id != $1 LIMIT $3 OFFSET $4', [requestedUser, userId, limit, offset]);
     return result;
 }
 
 const retrieveUserGift = async (userId, giftName) => {
     userId = userId.replace(/^"|"$/g, '');
-    result = await db.query('SELECT * FROM gifts WHERE gift_name = $1 AND user_id = $2', [giftName, userId]);
+    const result = await db.query('SELECT * FROM gifts WHERE gift_name = $1 AND user_id = $2', [giftName, userId]);
     return result;
 }
 
 const retrieveGiftById = async (userId, giftId) => {
     userId = userId.replace(/^"|"$/g, '');
-    result = await db.query('SELECT * FROM gifts WHERE id = $2 AND gifted_user_id != $1', [userId, giftId]);
+    const result = await db.query('SELECT * FROM gifts WHERE id = $2 AND gifted_user_id != $1', [userId, giftId]);
     return result;
 }
 
 const createGift = async (userId, itemId, gifted_user_id, expiration_date, gift_name) => {
     userId = userId.replace(/^"|"$/g, '');
-    gifted_user_id = gifted_user_id.replace(/^"|"$/g, '');
+    let gifted_user_id = gifted_user_id.replace(/^"|"$/g, '');
     try {
         await db.query('BEGIN');
         await db.query('INSERT INTO gifts (item_id, gift_name, gifted_user_id, is_delivered, expiration_date, user_id) VALUES ($1, $2, false ,$3, $4)', [itemId, gift_name, gifted_user_id, expiration_date]);
@@ -56,30 +56,30 @@ const completeGift = async (giftId) => {
 }
 
 const retrieveImplicationRequests = async (giftId, limit, offset) => {
-    result = await db.query('SELECT * FROM user_gifts WHERE gift_id = $1 AND is_implicated = false LIMIT $2 OFFSET $3', [giftId, limit, offset]);
+    const result = await db.query('SELECT * FROM user_gifts WHERE gift_id = $1 AND is_implicated = false LIMIT $2 OFFSET $3', [giftId, limit, offset]);
     return result;
 }
 
 const retrieveImplications = async (giftId, limit, offset) => {
-    result = await db.query('SELECT * FROM user_gifts WHERE gift_id = $1 AND is_implicated = true LIMIT $2 OFFSET $3', [giftId, limit, offset]);
+    const result = await db.query('SELECT * FROM user_gifts WHERE gift_id = $1 AND is_implicated = true LIMIT $2 OFFSET $3', [giftId, limit, offset]);
     return result;
 }
 
 const retrieveUserImplications = async (userId, limit, offset) => {
     userId = userId.replace(/^"|"$/g, '');
-    result = await db.query('SELECT * FROM user_gifts WHERE user_id = $1 AND is_implicated = true LIMIT $2 OFFSET $3', [userId, limit, offset]);
+    const result = await db.query('SELECT * FROM user_gifts WHERE user_id = $1 AND is_implicated = true LIMIT $2 OFFSET $3', [userId, limit, offset]);
     return result;
 }
 
 const retrieveUserImplicationsRequested = async (userId, limit, offset) => {
     userId = userId.replace(/^"|"$/g, '');
-    result = await db.query('SELECT * FROM user_gifts WHERE user_id = $1 AND is_implicated = false LIMIT $2 OFFSET $3', [userId, limit, offset]);
+    const result = await db.query('SELECT * FROM user_gifts WHERE user_id = $1 AND is_implicated = false LIMIT $2 OFFSET $3', [userId, limit, offset]);
     return result;
 }
 
 const retrieveImplication = async (userId, giftId) => {
     userId = userId.replace(/^"|"$/g, '');
-    result = await db.query('SELECT * FROM user_gifts WHERE gift_id = $1 AND user_id = $2', [giftId, userId]);
+    const result = await db.query('SELECT * FROM user_gifts WHERE gift_id = $1 AND user_id = $2', [giftId, userId]);
     return result;
 }
 
