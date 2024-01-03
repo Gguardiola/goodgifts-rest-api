b48e9ac73ec5862ab556d576260def53a9957c4a
Gx3 Studios <gabriel.guardiola@estudiantat.upc.edu>
2024-01-03 02:26:20 +0100

    Update gifts.js

--- routes/gifts.js
+++ routes/gifts.js
@@ -261,14 +261,12 @@ router.post('/complete',[
         const giftId = req.body.giftId;
         const requestedUser = req.body.userId;
         let user = await dbUsers.checkIfUserExists(userId);
-        let requestedUserCheck = await dbUsers.checkIfUserExists(requestedUser);
-        if(!user.rows.length > 0 || requestedUserCheck.rows.length > 0) {
+        if(!user.rows.length > 0) {
             console.log("Error: User NOT exists");
             return res.status(404).json({ success: false, message: 'User not found' });
         }
 
         if(userId == requestedUser) {
-
             let gift = await db.retrieveGiftById(userId, giftId);
             if (!gift.rows.length > 0) {
                 return res.status(404).json({ success: false, message: 'Gift not found' });
